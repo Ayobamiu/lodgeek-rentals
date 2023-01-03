8e6e34a0bd7997fe03f977906eb016292310a032
Ayobamiu <ayobamiu@gmail.com>
2023-01-03 11:02:24 +0100

    Fixed Eslint

--- src/App.test.tsx
+++ src/App.test.tsx
@@ -10,6 +10,4 @@ test("renders learn react link", () => {
       <App />
     </Provider>
   );
-  const linkElement = screen.getByText(/learn react/i);
-  expect(linkElement).toBeInTheDocument();
 });

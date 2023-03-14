500fa757ea2c89ffcefe44529fd970c27bac3008
Ayobamiu <ayobamiu@gmail.com>
2023-03-14 17:45:38 +0100

    Added description to properties

--- src/screens/DashboardTabs/AddProperty.tsx
+++ src/screens/DashboardTabs/AddProperty.tsx
@@ -12,6 +12,7 @@ export default function AddProperty() {
   const [rent, setRent] = useState(0);
   const [rentPer, setRentPer] = useState<RentType>("year");
   const [title, setTitle] = useState("");
+  const [description, setDescription] = useState("");
   const [location, setLocation] = useState("");
   const [address, setAddress] = useState("");
   const navigate = useNavigate();
@@ -33,7 +34,7 @@ export default function AddProperty() {
       location,
       address,
       createdDate: Date.now(),
-      description: "",
+      description,
       owner: loggedInUser?.email,
       id: generateFirebaseId(PROPERTY_PATH),
     };
@@ -109,9 +110,29 @@ export default function AddProperty() {
                       required
                       className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-green-500 border border-coolGray-200 rounded-lg shadow-input"
                       type="text"
-                      placeholder="e.g 2 Bedroom Furnished Flat"
+                      placeholder="Title of apartment"
                       onChange={(e) => setTitle(e.target.value)}
                       defaultValue={title}
+                    />
+                  </div>
+                </div>
+              </div>
+            </div>
+            <div className="py-6 border-b border-coolGray-100">
+              <div className="w-full md:w-9/12">
+                <div className="flex flex-wrap -m-3">
+                  <div className="w-full md:w-1/3 p-3">
+                    <p className="text-sm text-coolGray-800 font-semibold">
+                      Description
+                    </p>
+                  </div>
+                  <div className="w-full md:flex-1 p-3">
+                    <input
+                      className="w-full px-4 py-2.5 text-base text-coolGray-900 font-normal outline-none focus:border-green-500 border border-coolGray-200 rounded-lg shadow-input"
+                      type="text"
+                      placeholder="Description of apartment"
+                      onChange={(e) => setDescription(e.target.value)}
+                      defaultValue={description}
                     />
                   </div>
                 </div>

0bfb36da5583821cee857aa2bb0443623807f9dd
Ayobamiu <ayobamiu@gmail.com>
2023-04-15 16:32:31 +0100

    Don't log user out on refresh

---
+++ src/hooks/useAppRedirects.ts
@@ -0,0 +1,129 @@
+import { useEffect } from "react";
+import { useNavigate, useParams } from "react-router-dom";
+import { toast } from "react-toastify";
+import { setSelectedCompany } from "../app/features/companySlice";
+import { selectUser } from "../app/features/userSlice";
+import { useAppDispatch, useAppSelector } from "../app/hooks";
+import { getCompany, getUserDefaultCompany } from "../firebase/apis/company";
+import useQuery from "./useQuery";
+
+const useAppRedirects = () => {
+  let query = useQuery();
+  const navigate = useNavigate();
+  const loggedInUser = useAppSelector(selectUser);
+  const redirectFromQuery = query.get("redirect") as string;
+  const emailFromQuery = query.get("email") as string;
+  const dispatch = useAppDispatch();
+  let { companyId } = useParams();
+
+  useEffect(() => {
+    resolveAuthAndLoadCompany();
+  }, [loggedInUser?.email, companyId]);
+  /* 
+  fnct resolveAuthAndLoadCompany
+    if no auth
+      navigate to auth page
+    else 
+      if user has no userType
+        send to Go to registration page
+      else
+        if companyId
+          load company with companyId
+          if user has access to the company
+            setSelectedCompany
+          else 
+            show no-access indicator 
+            send to select-account page 
+        else 
+          if defaultCompany
+            setSelectedCompany
+          else
+            send to select-account page
+  */
+
+  /**
+   * Evaluates user's auth status and companies to display company info on the dashboard
+   */
+  const resolveAuthAndLoadCompany = async () => {
+    //if no auth
+    if (!loggedInUser?.email) {
+      //navigate to auth page
+      gotoAuthPage();
+    } else {
+      //if user has no userType
+      if (!loggedInUser.userType) {
+        //Go to registration page.
+        gotoRegistrationPage();
+      } else {
+        //if companyId
+        if (companyId) {
+          //load company with companyId
+          const company = await getCompany(companyId);
+          const userHasAccess = [
+            company.primaryOwner,
+            company.createdBy,
+            ...company.team,
+          ].includes(loggedInUser.email);
+
+          //if user has access to the company
+          if (userHasAccess) {
+            //setSelectedCompany
+            dispatch(setSelectedCompany(company));
+          } else {
+            //show no-access indicator
+            toast.error("You don't access to this account");
+            //send to select-account page
+            sendToSelectAccountPage();
+          }
+        } else {
+          const defaultCompany = await getUserDefaultCompany(
+            loggedInUser.email
+          );
+          //if defaultCompany
+          if (defaultCompany) {
+            //setSelectedCompany
+            dispatch(setSelectedCompany(defaultCompany));
+          } else {
+            //send to select-account page
+            sendToSelectAccountPage();
+          }
+        }
+      }
+    }
+
+    function sendToSelectAccountPage() {
+      //send to select-account page
+      if (redirectFromQuery && redirectFromQuery !== "null") {
+        return navigate(`/select-accounts?redirect=${redirectFromQuery || ""}`);
+      } else {
+        return navigate(`/select-accounts`);
+      }
+    }
+
+    function gotoRegistrationPage() {
+      //Go to registration page.
+      if (redirectFromQuery && redirectFromQuery !== "null") {
+        return navigate(`/get-started?redirect=${redirectFromQuery}`);
+      } else {
+        return navigate(`/get-started`);
+      }
+    }
+
+    function gotoAuthPage() {
+      if (redirectFromQuery) {
+        navigate(
+          `/auth?redirect=${
+            redirectFromQuery && redirectFromQuery !== "null"
+              ? redirectFromQuery
+              : ""
+          }&email=${emailFromQuery}`
+        );
+      } else {
+        navigate("/auth");
+      }
+    }
+  };
+
+  return { resolveAuthAndLoadCompany };
+};
+export default useAppRedirects;
--- src/screens/DashboardTabs/ReviewsOnRentalRecord.tsx
+++ src/screens/DashboardTabs/ReviewsOnRentalRecord.tsx
@@ -9,7 +9,7 @@ import useRentalRecords from "../../hooks/useRentalRecords";
 import { ReviewItem } from "./ReviewItem";
 
 export const ReviewsOnRentalRecord = () => {
-  let { id } = useParams();
+  let { rentalRecordId } = useParams();
   const navigate = useNavigate();
   const { rentReviews } = useAppSelector(selectRentReview);
   const { loadRentalReviews } = useRentalRecords();
@@ -18,15 +18,15 @@ export const ReviewsOnRentalRecord = () => {
   const loggedInUser = useAppSelector(selectUser);
 
   useEffect(() => {
-    if (id) {
+    if (rentalRecordId) {
       (async () => {
         setLoading(true);
-        await loadRentalReviews(id).finally(() => {
+        await loadRentalReviews(rentalRecordId).finally(() => {
           setLoading(false);
         });
       })();
     }
-  }, [id]);
+  }, [rentalRecordId]);
 
   return (
     <div className=" p-6 bg-white border border-gray-200 rounded-lg shadow dark:bg-gray-800 dark:border-gray-700">
@@ -39,7 +39,9 @@ export const ReviewsOnRentalRecord = () => {
             type="submit"
             title="Start Rent Review"
             onClick={() => {
-              navigate(`/dashboard/rentalRecords/${id}/rent-review/new`);
+              navigate(
+                `/dashboard/rentalRecords/${rentalRecordId}/rent-review/new`
+              );
             }}
             className="flex gap-x-3 items-center justify-center text-white bg-green-700 disabled:bg-gray-500 disabled:cursor-not-allowed hover:bg-green-800 focus:ring-4 focus:outline-none focus:ring-green-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-green-600 dark:hover:bg-green-700 dark:focus:ring-green-800"
           >
